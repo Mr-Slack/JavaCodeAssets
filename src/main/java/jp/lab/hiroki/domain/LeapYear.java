956dae819e44674ec585b382796c6ed4dc67b5b0
MINT <j.hiroki.matsushita@gmail.com>
2017-10-01 21:20:27 +0900

    ドメインクラス:うるう年 追加

---
+++ src/main/java/jp/lab/hiroki/domain/LeapYear.java
@@ -0,0 +1,32 @@
+package jp.lab.hiroki.domain;
+
+/**
+ * うるう年クラス
+ *
+ * @author HIROKI
+ *
+ */
+public class LeapYear {
+
+    private LeapYear() {
+    }
+
+    /**
+     * うるう年かどうかを判定
+     *
+     * @param year
+     *            処理判定西暦
+     * @return うるう年の場合 {@code true} そうでない場合 {@code false}
+     */
+    public static boolean isLeapYear(final int year) {
+        if (year % 4 == 0) {
+            // うるう年の例外
+            if (year % 100 == 0 && year % 400 != 0) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/jp/lab/hiroki/domain/LeapYearTest.java
@@ -0,0 +1,45 @@
+package jp.lab.hiroki.domain;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeapYearTest {
+
+    @Test
+    public void 西暦が４の倍数でない場合falseが返ること() throws Exception {
+        // Exercise
+        boolean actual = LeapYear.isLeapYear(1999);
+        boolean expected = false;
+        // Verify
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 西暦が４の倍数でかつ１００の倍数でかつ４００の倍数でない場合falseが返ること() throws Exception {
+        // Exercise
+        boolean actual = LeapYear.isLeapYear(2100);
+        boolean expected = false;
+        // Verify
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 西暦が４の倍数でかつ１００の倍数でかつ４００の倍数の場合trueが返ること() throws Exception {
+        // Exercise
+        boolean actual = LeapYear.isLeapYear(2400);
+        boolean expected = true;
+        // Verify
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 西暦が４の倍数でかつ１００の倍数でない場合trueが返ること() throws Exception {
+        // Exercise
+        boolean actual = LeapYear.isLeapYear(2004);
+        boolean expected = true;
+        // Verify
+        assertThat(actual, is(expected));
+    }
+}
